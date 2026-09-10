a3f2ddb764e4755ee81d72d1acdcd021de8049f1
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-10 11:00:58 +0300

    refactor: restructure Header component and update styles

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,13 @@
-function Header(){
-    return <h1>Crypto Analytics</h1>
+function Header() {
+    return (
+        <header>
+            <h1>Crypto Analytics</h1>
+            <nav>
+                <a>Dashboard</a>
+                <a>Markets</a>
+            </nav>
+        </header>
+    )
 }
 
-export default Header;
+export default Header
