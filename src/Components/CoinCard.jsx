a806b3b8d74e47c53ddbb1e89d49d768a29da4af
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-14 09:12:43 +0300

    design changes

--- src/Components/CoinCard.jsx
+++ src/Components/CoinCard.jsx
@@ -1,10 +1,9 @@
+import CoinLogo from "./CoinLogo.jsx"
+
 function CoinCard({ coin , onSelect}) {
     return (
         <div className="coin-card" onClick={onSelect}>
-            <h3>
-                {coin.name}
-                <span>{coin.symbol.toUpperCase()}</span>
-            </h3>
+            <h3><CoinLogo coinId={coin.id} />{coin.name}<span>{coin.symbol.toUpperCase()}</span></h3>
             <p>${coin.current_price.toFixed(2)}</p>
             <p>${formatMarketCap(coin.market_cap)}</p>
             <p className={coin.price_change_percentage_24h >= 0 ? "positive" : "negative"}>
@@ -16,18 +15,9 @@ function CoinCard({ coin , onSelect}) {
 }
 
 function formatMarketCap(value) {
-    if (value >= 1_000_000_000_000) {
-        return (value / 1_000_000_000_000).toFixed(1) + "T";
-    }
-
-    if (value >= 1_000_000_000) {
-        return (value/1_000_000_000).toFixed(1)+"B";
-    }
-
-    if (value >= 1_000_000) {
-        return  (value/1_000_000).toFixed(1)+"M";
-    }
-
+    if (value >= 1_000_000_000_000) return (value / 1_000_000_000_000).toFixed(1) + "T";
+    if (value >= 1_000_000_000) return (value / 1_000_000_000).toFixed(1) + "B";
+    if (value >= 1_000_000) return (value / 1_000_000).toFixed(1) + "M";
     return value
 }
 export default CoinCard
