acf8c44a3af171a5c753de3a871622cd5c226bb8
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-16 22:50:49 +0300

    design changes

--- src/Components/CoinLogo.jsx
+++ src/Components/CoinLogo.jsx
@@ -1,12 +1,29 @@
 import CryptoIcon from "./CryptoIcon.jsx"
 
-function CoinLogo({ coinId }) {
-    const symbolById = {
-        bitcoin: "BTC",
-        ethereum: "ETH",
-        solana: "SOL",
-    }
+const symbolById = {
+    bitcoin: "BTC",
+    ethereum: "ETH",
+    solana: "SOL",
+    binancecoin: "BNB",
+    ripple: "XRP",
+    cardano: "ADA",
+    "avalanche-2": "AVAX",
+    dogecoin: "DOGE",
+    polkadot: "DOT",
+    chainlink: "LINK",
+    uniswap: "UNI",
+    litecoin: "LTC",
+    cosmos: "ATOM",
+    near: "NEAR",
+    aptos: "APT",
+    arbitrum: "ARB",
+    optimism: "OP",
+    sui: "SUI",
+    pepe: "PEPE",
+    filecoin: "FIL",
+}
 
+function CoinLogo({ coinId }) {
     return <span className="coin-logo"><CryptoIcon symbol={symbolById[coinId] || "BTC"} /></span>
 }
 
