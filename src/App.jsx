db40d5ebbd280aeebf57608e88683e167fca59ae
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-17 21:32:09 +0300

    design changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import DashboardPage from "./Components/DashboardPage.jsx";
 import MarketsPage from "./Components/MarketsPage.jsx";
 import AiPredictPage from "./Components/AiPredictPage.jsx";
 
-
 function safeFetch(url) {
     return fetch(url).then(async (response) => {
         const text = await response.text();
