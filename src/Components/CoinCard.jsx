06523b287cb53fd2bf4dfb2bb83d58cd70ef6786
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-21 22:06:27 +0300

    design changes

--- src/Components/CoinCard.jsx
+++ src/Components/CoinCard.jsx
@@ -14,7 +14,7 @@ function CoinCard({ coin , onSelect}) {
         </div>
     )
 }
-
+ 
 function formatMarketCap(value) {
     if (value >= 1_000_000_000_000) return (value / 1_000_000_000_000).toFixed(1) + "T";
     if (value >= 1_000_000_000) return (value / 1_000_000_000).toFixed(1) + "B";
